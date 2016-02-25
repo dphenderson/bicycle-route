42025668b0fb97366a0cf16d443c60ebfd889b5b
David P Henderson <dphenderson2@gmail.com>
2016-02-25 12:54:45 -0800

    Modify Projection and SVG Grouping
    Change projection from albers to conicConformal. Set values on
    projection for parallels, rotation, center, translation, and scale. Add
    group element and give id attribute of 'map'. Calculate translation
    value to draw map centered in window on the Geographic Center of the
    Contiguous United States. Change variable instance that is receiving d3
    output to draw map.

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,10 +1,15 @@
 (function(window, document, undefined) {
 
-	if (!Array.prototype.includes) {
-		Array.prototype.includes = function(searchElement /*, fromIndex*/ ) {
-			'use strict';
-			var O = Object(this);
-			var len = parseInt(O.length) || 0;
+	/* Polyfill
+	 *
+	 * Noticed that map was not drawing correctly in my older iPhone browser.
+	 * Investigation revealed that the includes method was undefined.  
+	 *
+	 */ 
+
+	if (!Array.prototype.includes) { Array.prototype.includes =
+		function(searchElement /*, fromIndex*/ ) { 'use strict'; var O =
+			Object(this); var len = parseInt(O.length) || 0;
 			if (len === 0) {
 				return false;
 			}
@@ -31,9 +36,12 @@
 
   var width = window.innerWidth,
       height = window.innerHeight,
-	  proj = d3.geo.albers(),
+	  proj = d3.geo.conicConformal().parallels([24.5, 49.5]).rotate([96,0]).center([-98.583333, 39.833333]).translate([0,0]).scale(2000),
 	  path = d3.geo.path().projection(proj),
-      svg = d3.select('body').append('svg').attr('width', width).attr('height', height);
+      svg = d3.select('body').append('svg').attr('width', width).attr('height', height),
+	  g = svg.append('g').attr('id', 'map');
+
+	  proj.translate([(width / 2) - proj(proj.center())[0],(height / 2) - proj(proj.center())[1]]);
 
       d3.json('/assets/json/north_america.json', function(error, na) {
         if (error) {
@@ -41,10 +49,10 @@
         }
 		var states_passed = ['VA', 'KY', 'IN', 'IL', 'IA', 'NE', 'WY', 'ID', 'OR', 'CA'];
 
-		svg.selectAll('.state').data(topojson.feature(na, na.objects.usa).features.filter(function(d) { return states_passed.includes(d.id); })).enter().append('path').attr('id', function(d) { return d.id; }).attr('class', function(d) { return 'state ' + d.id; }).attr('d', path);
-        svg.append('path').datum(topojson.mesh(na, na.objects.north_america)).attr('d', path).attr('class', 'intl-boundaries');
-        svg.append('path').datum(topojson.mesh(na, na.objects.usa)).attr('d', path).attr('class', 'state-boundaries');
-        svg.append('path').datum(topojson.feature(na, na.objects.waypoints)).attr('d', path.pointRadius(2)).attr('class', 'waypoint');
-        svg.append('path').datum(topojson.mesh(na, na.objects.routes)).attr('d', path).attr('class', 'route');
+		g.selectAll('.state').data(topojson.feature(na, na.objects.usa).features.filter(function(d) { return states_passed.includes(d.id); })).enter().append('path').attr('id', function(d) { return d.id; }).attr('class', function(d) { return 'state ' + d.id; }).attr('d', path);
+        g.append('path').datum(topojson.mesh(na, na.objects.north_america)).attr('d', path).attr('class', 'intl-boundaries');
+        g.append('path').datum(topojson.mesh(na, na.objects.usa)).attr('d', path).attr('class', 'state-boundaries');
+        g.append('path').datum(topojson.feature(na, na.objects.waypoints)).attr('d', path.pointRadius(2)).attr('class', 'waypoint');
+        g.append('path').datum(topojson.mesh(na, na.objects.routes)).attr('d', path).attr('class', 'route');
       });
 })(window, document);
